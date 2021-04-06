90621e3af9724aa4a990c452ab1ad1bb2ab35317
radimcapak <radim.capak@gmail.com>
2021-04-06 14:58:49 +0200

    Match check LB to TR

--- src/logic.js
+++ src/logic.js
@@ -19,27 +19,30 @@ export const logic = {
 
 	checkMatches: function(currMove, player) {
 		const ID_TEMPLATE = "abcdefghijklmnopqrstuvwxyz";
-		let playerMatches = 0;
 
 		let [y, x] = currMove.split("-");
 		y = Math.abs(y);
 		let count = 0;
+		let playerMatches = 1;
 
-		function topRight(count) {
+
+		function leftBottomToTopRight(count) {
 			count++;
+			const topRightCell = `${y - count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) + count]}`;
+			const bottomLeftCell = `${y + count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) - count]}`;
 
-			const unitTopRight = `${y - count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) + count]}`;
+			if(this.moves[`player${player}`].includes(topRightCell) ||
+				this.moves[`player${player}`].includes(bottomLeftCell)) {
 
-			if(this.moves[`player${player}`].includes(unitTopRight)) {
-
-				console.log("has match");
 				playerMatches++;
+				console.log("has match", playerMatches);
 
-				return topRight.call(this, count);
+
+				return leftBottomToTopRight.call(this, count);
 			}
 			
 		}
-		topRight.call(this, count);
+		leftBottomToTopRight.call(this, count);
 
 
 

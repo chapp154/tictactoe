509732c9e5715263af29ea8bd75cd6aaf012835c
Radim Capak <radim.capak@gmail.com>
2021-04-07 19:56:21 +0200

    Trying to use useeffect to store data

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import Header from "./Header";
 import Grid from "./Grid";
-import { useState, useEffect } from "react";
-import { logic } from "./logic";
+import { useState } from "react";
+//import { logic } from "./logic";
 
 function App() {
 
 	const [fieldEvent, setFieldEvent] = useState(false);
 	const [currMove, setcurrMove] = useState(null);
-	const [player, setPlayer] = useState(1);
-	const [gameIsRunning, setGameIsRunning] = useState(true);
 
 
 	function handleMove(event) {
 
-		if(event.target.innerText || !gameIsRunning) return;
+		if(event.target.innerText) return;
 
 		setFieldEvent(event);
 		setcurrMove(event.target.id);
 
 	}
 
-	useEffect(() => {
-		if(fieldEvent) {
-			logic.placeSymbol(player, fieldEvent);
-			logic.movesMemory(currMove, player);
 
-			if(logic.checkMatches(currMove, player)) {
-				setGameIsRunning(false, []);
-				console.log("Victory!!");
-			}
 
-			setPlayer((player) => player === 1 ? 2 : 1);
+	// useEffect(() => {
+	// 	if(fieldEvent) {
+	// 		logic.placeSymbol(player, fieldEvent);
+	// 		logic.movesMemory(currMove, player);
 
-		}
-	}, [fieldEvent]);
+	// 		if(logic.checkMatches(currMove, player)) {
+	// 			setGameIsRunning(false, []);
+	// 			console.log("Victory!!");
+	// 		}
+
+	// 		setPlayer((player) => player === 1 ? 2 : 1);
+
+	// 	}
+	// }, [fieldEvent]);
 
 
 	return (
     <div className="App">
-		<Header player = {player}/>
+		<Header fieldEvent = {fieldEvent} currMove = {currMove}/>
 		<Grid handleMove = {handleMove}/>
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,27 @@
-const Header = ({player}) => {
+import { useState, useEffect } from "react";
 
+const Header = ({fieldEvent, currMove}) => {
 
+	const [player, setPlayer] = useState(1);
+	const [moves1, setMoves1] = useState([])
+
+	useEffect(() => {
+
+		if(!fieldEvent) return;
+
+		fieldEvent.target.textContent = player === 1 ? "X" : "O";
+
+		//setMoves({[`player${player}`]: [...moves[`player${player}`], currMove]});
+		if(player === 1) {
+			setMoves1((moves1) => [...moves1, currMove])
+
+		}
+
+		setPlayer((player) => player === 1 ? 2 : 1);
+
+	}, [fieldEvent]);
+
+	console.log(player, moves1);
 
 	return (
 		<header>

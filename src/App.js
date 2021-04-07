18f34c2d25f6970c3154d6229ac71e611ad98f1b
radimcapak <radim.capak@gmail.com>
2021-04-07 15:00:22 +0200

    Score check in all directions

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ function App() {
 	const [fieldEvent, setFieldEvent] = useState(false);
 	const [currMove, setcurrMove] = useState(null);
 	const [player, setPlayer] = useState(1);
+	const [gameIsRunning, setGameIsRunning] = useState(true);
 
 
-	const handleMove = (event) => {
+	function handleMove(event) {
 
-		if(event.target.innerText) return;
+		if(event.target.innerText || !gameIsRunning) return;
 
 		setFieldEvent(event);
 		setcurrMove(event.target.id);
@@ -23,7 +24,11 @@ function App() {
 		if(fieldEvent) {
 			logic.placeSymbol(player, fieldEvent);
 			logic.movesMemory(currMove, player);
-			logic.checkMatches(currMove, player)
+
+			if(logic.checkMatches(currMove, player)) {
+				setGameIsRunning(false, []);
+				console.log("Victory!!");
+			}
 
 			setPlayer((player) => player === 1 ? 2 : 1);
 
--- src/logic.js
+++ src/logic.js
@@ -5,50 +5,77 @@ export const logic = {
 		player2: [],
 	},
 
-	placeSymbol: (player, fieldEvent) => {
+	placeSymbol: (player, fieldEvent) => fieldEvent.target.textContent = player === 1 ? "X" : "O",
 
-		fieldEvent.target.textContent = player === 1 ? "X" : "O";
-
-	},
-
-	movesMemory: function(currMove, player) {
-
-		this.moves[`player${player}`] = [...this.moves[`player${player}`], currMove];
-
-	},
+	movesMemory: function(currMove, player) {this.moves[`player${player}`] = [...this.moves[`player${player}`], currMove]},
 
 	checkMatches: function(currMove, player) {
 		const ID_TEMPLATE = "abcdefghijklmnopqrstuvwxyz";
 
 		let [y, x] = currMove.split("-");
 		y = Math.abs(y);
-		let count = 0;
+		let countGlobal = 0;
 		let playerMatches = 1;
+		let playerWon = false;
 
 		function checkTeplate(fn, count, direction1, direction2) {
 			if(this.moves[`player${player}`].includes(direction1) ||
 				this.moves[`player${player}`].includes(direction2)) {
 
 				playerMatches++;
-				console.log("has match", playerMatches);
+				console.log("has match", playerMatches, "count:"+count);
+
+				if(playerMatches >= 4) {
+					playerWon = true;
+					return;
+				}
 
 				return fn.call(this, count);
+			} else {
+				playerMatches = 1;
+				countGlobal = 0;
 			}
 		}
 
 
-		function leftBottomToTopRight(count) {
+		(function crossAxis1(count) {
 			count++;
 			const topRightCell = `${y - count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) + count]}`;
 			const bottomLeftCell = `${y + count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) - count]}`;
 
-			checkTeplate.call(this, leftBottomToTopRight, count, topRightCell, bottomLeftCell);
+			checkTeplate.call(this, crossAxis1, count, topRightCell, bottomLeftCell);
 			
-		}
-		leftBottomToTopRight.call(this, count);
+		}).call(this, countGlobal);
+
+		(function crossAxis2(count) {
+			count++;
+			const topLeftCell = `${y - count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) - count]}`;
+			const bottomRightCell = `${y + count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) + count]}`;
+
+			checkTeplate.call(this, crossAxis2, count, topLeftCell, bottomRightCell);
+			
+		}).call(this, countGlobal);
+
+		(function xAxis(count) {
+			count++;
+			const xAxisMinus = `${y}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) - count]}`;
+			const xAxisPlus = `${y}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x) + count]}`;
+
+			checkTeplate.call(this, xAxis, count, xAxisMinus, xAxisPlus);
+			
+		}).call(this, countGlobal);
+
+		(function yAxis(count) {
+			count++;
+			const yAxisMinus = `${y - count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x)]}`;
+			const yAxisPlus = `${y + count}-${ID_TEMPLATE[ID_TEMPLATE.indexOf(x)]}`;
+
+			checkTeplate.call(this, yAxis, count, yAxisMinus, yAxisPlus);
+			
+		}).call(this, countGlobal);
 
 
 
-		console.log(x, y);
+		return playerWon;
 	}
 }
